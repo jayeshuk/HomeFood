757c0340c64e54c4f94dd08a3945fdcb62c9aff1
Hariom210799 <hariomlapshetwar@gmail.com>
2020-10-15 22:35:32 +0530

    Menu Screen

--- src/screens/MakerScreens/menu/index.js
+++ src/screens/MakerScreens/menu/index.js
@@ -1,15 +1,24 @@
 import React, {Component} from 'react';
-import {Dimensions, View, StyleSheet, ScrollView} from 'react-native';
+import {
+  Dimensions,
+  View,
+  StyleSheet,
+  ScrollView,
+  SectionList,
+  TouchableOpacity,
+} from 'react-native';
 import {
   Divider,
   Icon,
   Layout,
+  CheckBox,
   Text,
   TopNavigation,
   TopNavigationAction,
   Button,
   List,
-  ListItem,
+  MenuItem,
+  Card,
 } from '@ui-kitten/components';
 
 const SLIDER_WIDTH = Dimensions.get('window').width;
@@ -21,58 +30,153 @@ const data = new Array(8).fill({
   description: 'Description for Item',
 });
 
-const renderItemAccessory = (props) => <Button size="tiny">FOLLOW</Button>;
-
-const renderItemIcon = (props) => <Icon {...props} name="person" pack="eva" />;
-
-const renderItem = ({item, index}) => (
-  <ListItem
-    title={`${item.title} ${index + 1}`}
-    description={`${item.description} ${index + 1}`}
-    accessoryLeft={renderItemIcon}
-    accessoryRight={renderItemAccessory}
-  />
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
 );
 
-class Menu extends Component {
-  render() {
+const ForwardIcon = (props) => (
+  <Icon {...props} name="arrow-ios-forward" pack="eva" />
+);
+
+export default function Menu() {
+  const DATA = [
+    {
+      title: 'Starters',
+      data: ['Masala Papad', 'Oats', 'Banana Chips'],
+    },
+    {
+      title: 'Pickels and Chutneys',
+      data: ['Mango Pickle', 'Jawas Chutney', 'Groundnut Chutney'],
+    },
+    {
+      title: 'Breads',
+      data: ['Chapatai', 'Bhakri', 'Phulka'],
+    },
+    {
+      title: 'Main Course',
+      data: ['Bhendi Masala', 'Dal Curry'],
+    },
+    {
+      title: 'Rice',
+      data: ['Plain Rice', 'Pulao', 'Masala Rice'],
+    },
+    {
+      title: 'Snacks',
+      data: ['French Fries', 'Kaande Bhaje', 'Baked Papad'],
+    },
+    {
+      title: 'Salads',
+      data: ['Mix Salad', 'Onion Salad'],
+    },
+  ];
+  const [activeChecked, setActiveChecked] = React.useState(false);
+  const renderItem = (info) => {
     return (
-      <Layout style={styles.container} level="1">
-        <TopNavigation
-          // style={{paddingLeft: 20}}
-          title={(TextProps) => {
+      <Card
+        style={styles.item}
+        status="warning"
+        header={(headerProps) => (
+          <View style={{backgroundColor: '#DDDDDD'}}>
+            <MenuItem
+              accessoryLeft={(props) => (
+                <Icon
+                  {...props}
+                  style={{fontSize: 25, color: 'grey'}}
+                  name="local-dining"
+                  pack="material"
+                />
+              )}
+              accessoryRight={ForwardIcon}
+              onPress={() => navigation.navigate.goBack()}
+              title={(evaProps) => (
+                <Text
+                  {...evaProps}
+                  category="h5"
+                  style={{
+                    padding: 6,
+                    color: 'grey',
+                    width: '80%',
+                  }}>
+                  {info.item.title}
+                </Text>
+              )}
+            />
+          </View>
+        )}>
+        <View style={styles.listcontainer}>
+          {info.item.data.map((i, index) => {
             return (
-              <Text category="h2" status="primary">
-                Menu
-              </Text>
+              <CheckBox
+                key={index}
+                status="success"
+                style={styles.checkbox}
+                checked={activeChecked}
+                onChange={(nextChecked) => setActiveChecked(nextChecked)}>
+                {(evaProps) => (
+                  <Text category="s2" style={{marginLeft: 6, color: 'grey'}}>
+                    {i}
+                  </Text>
+                )}
+              </CheckBox>
             );
-          }}
-          alignment="start"
-        />
-        <Divider />
-        <Layout style={{flex: 1}} level="1">
-          {/* <Text category="h5">This is your Menu!</Text> */}
-          <List
-            style={{
-              flex: 1,
-              width: '100%',
-              position: 'absolute',
-            }}
-            data={data}
-            renderItem={renderItem}
-          />
-        </Layout>
-      </Layout>
+          })}
+        </View>
+      </Card>
     );
-  }
+  };
+
+  return (
+    <Layout style={styles.container} level="1">
+      <TopNavigation
+        title={(TextProps) => {
+          return (
+            <Text category="h2" status="primary">
+              Menu
+            </Text>
+          );
+        }}
+        alignment="start"
+      />
+      <Divider />
+      <Layout style={{flex: 1, width: '100%'}} level="1">
+        <List
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+          data={DATA}
+          renderItem={renderItem}
+        />
+      </Layout>
+    </Layout>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'red',
+    // backgroundColor: 'red',
+    // alignItems: 'center',
+  },
+  contentContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+  },
+  item: {
+    marginVertical: 4,
+    borderRadius: 10,
+  },
+  checkbox: {
+    width: '100%',
+    padding: 4,
+  },
+  listcontainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'space-around',
+    // backgroundColor: 'red',
+    flexWrap: 'wrap',
   },
 });
-
-export default Menu;
